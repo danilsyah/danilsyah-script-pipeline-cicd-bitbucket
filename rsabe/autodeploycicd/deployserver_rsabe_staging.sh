6b428cb4d94f7ad97d47d055b6f262c82c11add0
Danilsyah <danilsyaharihardjo@gmail.com>
2023-11-16 13:11:06 +0700

    update

--- rsabe/autodeploycicd/deployserver_rsabe_staging.sh
+++ rsabe/autodeploycicd/deployserver_rsabe_staging.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+#pipelines remote ssh server onpremise IP 192.168.0.54
 cd /var/www/html/ihospital-rsabe-simulasi
 tanggal=$(date +'%Y%m%d_%H%M')
 git checkout -b pull_$tanggal
-git pull origin rskt
+git pull origin rsabe
 exit
